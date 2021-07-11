fb1a72ebcf6bdbd78f2e5cc43d38e114c298ea26
Shaziya2020 <shaziyaismail.m@gmail.com>
2021-07-11 19:00:03 +0100

    Commit change

--- R/Calc_Dist_Mat.R
+++ R/Calc_Dist_Mat.R
@@ -1,8 +1,18 @@
 #Function to compute distance matrix
+#' Title
+#'
+#' @param VEC
+#'
+#' @return
+#' @export
+#'
+#' @examples:
 Calc_Dist_Mat <- function(VEC){
   N_atm <- (length(VEC)-1)/3
   MAT <- as.matrix((dist(t(matrix(VEC[2:length(VEC)], ncol=N_atm)), method='euclidean', upper=TRUE, diag=TRUE)))
   return(VEC)
 }
+
+
 
 
